4f588cd56601aad066b19db09d4f20741adef82d
Marcos Breno <123556594+mbrenoaf@users.noreply.github.com>
2023-02-15 15:43:46 -0300

    Add files via upload

---
+++ desafio_atribuicao.js
@@ -0,0 +1,9 @@
+console.log('Lista de Aprovados \n======================\n')
+let l = '1. Ana Silva'
+l += '\n2. Pedro Albuquerque'
+l += '\n3. Guilherme Pereira'
+l += '\n4. Rebeca França'
+l += '\n5. Marcos Breno'
+console.log(l, '\n\nPARABÉNS!')
+
+console.log('Lista de Aprovados\n=====================\n\n1. Ana Silva\n2. Pedro Albuquerque\n3. Guilherme Pereira\n4. Rebeca França\n5. Marcos Breno\n\nPARABÉNS!')
